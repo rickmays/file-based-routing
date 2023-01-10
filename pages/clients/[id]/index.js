208b9499e8a0c84f8580c535d537c32f7587ac9e
Rick Mays <8709540+rickmays@users.noreply.github.com>
2023-01-10 15:39:27 -0600

    completed follow-along with instructor for file-based-routing

--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -4,9 +4,25 @@ function ClientProjectsPage() {
   const router = useRouter();
 
   console.log(router.query);
+
+  function loadProjectHandler() {
+    // load data...
+
+    // string-based setting of paths
+    // router.push("/clients/max/projecta");
+    // router.replace("/clients/max/projecta");
+
+    // object-based setting of paths
+    router.push({
+      pathname: "/clients/[id]/[clientprojectid]",
+      query: { id: "max", clientprojectid: "projecta" },
+    });
+  }
+
   return (
     <div>
       <h1>The Projects of a Given Client</h1>
+      <button onClick={loadProjectHandler}>Load Project A</button>
     </div>
   );
 }
